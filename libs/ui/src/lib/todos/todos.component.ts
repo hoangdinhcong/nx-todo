d139cca9eb901af01e4629ed22d3f903e7dee8ac
Hoang Cong <hodico.iam@gmail.com>
2021-06-09 10:57:34 +0700

    init

---
+++ libs/ui/src/lib/todos/todos.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input } from '@angular/core';
+import { Todo } from '@myorg/data';
+
+@Component({
+  selector: 'myorg-todos',
+  templateUrl: './todos.component.html',
+  styleUrls: ['./todos.component.css']
+})
+export class TodosComponent {
+
+  @Input() todos!: Todo[];
+
+}
